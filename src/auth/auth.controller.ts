ab0a6b6d614884074e1e49c65b34aa5af677bdb5
João Pedro <joaoprl055@gmail.com>
2026-04-16 10:22:56 -0300

    Implementação de limites de tentativa de login e cadastro

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,16 +4,22 @@ import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { CreateUserDto } from 'src/Usuario/dto/create-user.dto';
 import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { Throttle } from '@nestjs/throttler';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 
 @Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService) { }
 
+    @UseGuards(ThrottlerGuard)
+    @Throttle({ default: { limit: 3, ttl: 60000 } })
     @Post('register')
     async createUser(@Body() createUserDto: CreateUserDto) {
         return this.authService.createUser(createUserDto)
     }
 
+    @UseGuards(ThrottlerGuard, AuthGuard('local'))
+    @Throttle({ default: { limit: 5, ttl: 60000 } })
     @UseGuards(AuthGuard('local'))
     @Post('login')
     async login(@Request() req, @Body() loginDto: LoginDto) {
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -8,23 +8,36 @@ import { AuthController } from './auth.controller';
 import { LocalStrategy } from './strategies/local.strategy'; // Importe a estratégia
 import { PrismaModule } from 'src/prisma.module';
 import { JwtStrategy } from './strategies/jwt.strategy';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
     imports: [
         UsuarioModule,
         PrismaModule,
         PassportModule,
+        ThrottlerModule.forRoot({
+            throttlers: [
+                {
+                    ttl: 60000,
+                    limit: 10,
+                },
+            ],
+        }),
         // Configuração do JWT:
         JwtModule.registerAsync({
             imports: [ConfigModule],
             inject: [ConfigService],
             useFactory: async (configService: ConfigService) => ({
                 secret: configService.get<string>('JWT_SECRET') || '123456',
-                signOptions: { expiresIn: '60m' }, 
+                signOptions: { expiresIn: '60m' },
             }),
         }),
     ],
     controllers: [AuthController],
-    providers: [AuthService, LocalStrategy, JwtStrategy], //estratégias 
+    providers: [AuthService, LocalStrategy, JwtStrategy, {
+        provide: APP_GUARD,
+        useClass: ThrottlerGuard,
+    },], //estratégias 
 })
 export class AuthModule { }
